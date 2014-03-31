cb59c6f8a301dbb874275c39218b557624a29f1a
Vadym Chepkov <vchepkov@gmail.com>
2014-03-31 01:19:58 -0400

    rotate webhook's logs

--- site/modules/r10k_webhook/manifests/init.pp
+++ site/modules/r10k_webhook/manifests/init.pp
@@ -40,4 +40,17 @@ class r10k_webhook (
     hasrestart => true,
     subscribe  => [ File['/etc/init.d/r10k_webhook'], File['/usr/local/sbin/r10k_webhook'], ],
   }
+
+  package { 'logrotate':
+    ensure  => installed,
+  }
+
+  file { '/etc/logrotate.d/r10k_webhook':
+    ensure => file,
+    owner   => root,
+    group   => root,
+    mode    => '0644',
+    content => template("${module_name}/logrotate.erb"),
+    require => Package['logrotate'],
+  }
 }
