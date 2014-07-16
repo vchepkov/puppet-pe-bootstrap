10acbeff177bdda3eaebfc8f52720028724a45e5
Vadym Chepkov <vchepkov@gmail.com>
2014-07-16 00:00:13 -0400

    modified for puppet 3.6

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,4 +1,15 @@
 ## site.pp ##
 
+# Workaround for annoying feature
+
+if versioncmp($::puppetversion,'3.6.1') >= 0 {
+
+  $allow_virtual_packages = hiera('allow_virtual_packages',false)
+
+  Package {
+    allow_virtual => $allow_virtual_packages,
+  }
+}
+
 # This includes all of our node definitions
 import 'nodes/*.pp'
